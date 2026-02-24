fd86dbfef75b0b85ab714f1e2f0031edd2d957af
amatya-aditya <adityaamatya75@gmail.com>
2026-02-23 19:36:01 -0600

    release: 1.0.1-beta.1

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,17 +1,17 @@
-import { readFileSync, writeFileSync } from "fs";
+import {readFileSync, writeFileSync} from "fs";
 
 const targetVersion = process.env.npm_package_version;
+if (!targetVersion) {
+	throw new Error("npm_package_version is not set");
+}
 
-// read minAppVersion from manifest.json and bump version to target version
+// Read minAppVersion from manifest.json and bump version to target version.
 const manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
-const { minAppVersion } = manifest;
+const {minAppVersion} = manifest;
 manifest.version = targetVersion;
 writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 
-// update versions.json with target version and minAppVersion from manifest.json
-// but only if the target version is not already in versions.json
-const versions = JSON.parse(readFileSync('versions.json', 'utf8'));
-if (!Object.values(versions).includes(minAppVersion)) {
-    versions[targetVersion] = minAppVersion;
-    writeFileSync('versions.json', JSON.stringify(versions, null, '\t'));
-}
+// Update versions.json for this exact target version.
+const versions = JSON.parse(readFileSync("versions.json", "utf8"));
+versions[targetVersion] = minAppVersion;
+writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
